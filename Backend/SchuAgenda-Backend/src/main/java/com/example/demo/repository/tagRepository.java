2dcb5b2e6ff1d837f220b274780c1740d36afadf
Gruppenuser IT-42 (IT-PTR-SL4) <sophie.fluri@students.lerbermatt.ch>
2021-10-01 09:10:25 +0200

    Revert "Controller_Backend"
    This reverts commit 44da5245ce06b3b67a082102e3baee80afdb29ea.

--- Backend/SchuAgenda-Backend/src/main/java/com/example/demo/repository/tagRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Example;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TagRepository extends JpaRepository<tag, Integer> {
-}
